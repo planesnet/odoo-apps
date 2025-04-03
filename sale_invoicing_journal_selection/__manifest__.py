02fbe9dbbda26cf4fc9564a178985331eccd7bc9
Luis Planes <luis@planesnet.com>
2025-04-03 19:57:40 +0200

    v 1.0.2

--- sale_invoicing_journal_selection/__manifest__.py
+++ sale_invoicing_journal_selection/__manifest__.py
@@ -5,7 +5,8 @@
     'summary': "Select the journal when you create invoices",
 
     'description': """
-Select the journal when you create invoices.
+Select the accounting journal to use when invoicing pending orders. 
+When you invoice pending orders en masse and want to record the generated invoice entries in a specific accounting journal, it's most practical to indicate this at the time the invoices are created.
     """,
 
     'author': "Planes Soluciones Informáticas",
@@ -17,8 +18,8 @@ Select the journal when you create invoices.
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting & Finance',
-    'version': '1.0.1',
-    'license': 'AGPL-3',
+    'version': '1.0.2',
+    'license': 'OPL-1',
     
 
     # any module necessary for this one to work correctly
@@ -27,14 +28,14 @@ Select the journal when you create invoices.
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'wizard/views.xml',
+        'views/views.xml',
     ],
 
 
 
     # Odoo store
     'images': ['static/description/banner.jpg'],    
-    'price': 0.0,
+    'price': 25.0,
     'currency': 'EUR',
 }
 
