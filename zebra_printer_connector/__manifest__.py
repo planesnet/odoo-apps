987fd299bc0ce44e0dc4a7fce686369118204d62
Luis Planes <luis@planesnet.com>
2025-09-11 18:43:22 +0200

    setting version

--- zebra_printer_connector/__manifest__.py
+++ zebra_printer_connector/__manifest__.py
@@ -15,7 +15,7 @@ Long description of module's purpose
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '17.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base','zebra_printer'],
