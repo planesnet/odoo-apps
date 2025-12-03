2da8b7912f0bf516ad142c702d7bd5c0da53876b
Laia Espinosa <laia@planesnet.com>
2025-12-03 15:52:32 +0100

    Modulos para limitar la creación de partners o productos desde los documentos para odoo V18

---
+++ restrict_create_partner_from_documents/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Restrict Create Partner From Documents',
+    'version': '18.0.1.0.0',    
+    'category': 'Customizations',
+    'summary': 'Restricts creating partners from sales, purchases and invoices.',
+    'author': 'Planesnet',
+    'website': 'https://www.planesnet.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_management',
+        'purchase',
+        'account',
+    ],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+}
