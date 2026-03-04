89898453c222716924c06f73c06a21b05360c654
richard-planesnet <richard@planesnet.com>
2026-03-04 12:21:54 +0100

    add module report_editor

---
+++ report_editor/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Editor de reportes Odoo 19",
+    'version': '19.0.1.0.0',
+    'author': "Planes Soluciones Informáticas",
+    'category': 'Web',
+    'depends': ['base', 'web', 'html_editor'],
+    'data': [
+        'views/res_company_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'report_editor/static/src/js/wysiwyg.js'
+        ],
+    },
+    'installable': True,
+    'license': 'OPL-1',
+}
