cf38c3180b88ddd4cef8a0d210268ee047e9e72d
richard-planesnet <richard@planesnet.com>
2026-03-04 15:39:20 +0100

    update report_editor

--- report_editor/__manifest__.py
+++ report_editor/__manifest__.py
@@ -4,9 +4,10 @@
     'version': '19.0.1.0.0',
     'author': "Planes Soluciones Informáticas",
     'category': 'Web',
-    'depends': ['base', 'web', 'html_editor'],
+    'depends': ['base', 'web', 'html_editor', 'account'],
     'data': [
         'views/res_company_views.xml',
+        'views/proforma_toggle.xml',
     ],
     'assets': {
         'web.assets_backend': [
